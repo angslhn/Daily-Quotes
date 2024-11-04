c792ec308d656e6dd7039e8b13b5212d9c1a7ed8
angslhn <angslhn@gmail.com>
2024-11-04 21:23:30 +0700

    Random quotes

--- main.js
+++ main.js
@@ -25,7 +25,6 @@ async function getQuote() {
   try {
     const response = await fetch("https://api.quotable.io/random");
     const data = await response.json();
-    console.log(data);
     return data;
   }
   catch (error) {
